ecc0ae61dc83cc273eabe15f20b607e63a6164ae
Jack <jack.walpole@bristol.ac.uk>
2017-06-22 18:07:29 +0100

    Bug fixes -- pseudo working code

--- icosahedron/icosahedron.py
+++ icosahedron/icosahedron.py
@@ -6,7 +6,7 @@ import numpy as np
 # hierarchy:
 # Icosphere -> Triangle -> Point
 
-class IcoSphere(level):
+class IcoSphere:
     """A collection of triangles and list of all points"""
     
     # maximum level for subdivision of the icosahedron
@@ -15,7 +15,7 @@ class IcoSphere(level):
     def __init__(self, level):       
         if type(level) is not int:
             raise TypeError('level must be an integer')
-        else if level > maxlevel:
+        elif level > self.maxlevel:
             raise Exception('level must be no larger than ' + str(maxlevel))
         
         self.level = level
@@ -30,18 +30,18 @@ class IcoSphere(level):
         t = (1.0 + np.sqrt(5.0)) / 2.0
         
         # add vertices
-        _addPoint(np.array([-1, t, 0]))
-        _addPoint(np.array([ 1, t, 0]))
-        _addPoint(np.array([-1,-t, 0]))
-        _addPoint(np.array([ 1,-t, 0]))
-        _addPoint(np.array([ 0,-1, t]))
-        _addPoint(np.array([ 0, 1, t]))
-        _addPoint(np.array([ 0,-1,-t]))
-        _addPoint(np.array([ 0,-1,-t]))
-        _addPoint(np.array([ t, 0,-1]))
-        _addPoint(np.array([ t, 0, 1]))
-        _addPoint(np.array([-t, 0,-1]))
-        _addPoint(np.array([-t, 0, 1]))
+        self._addPoint(np.array([-1, t, 0]))
+        self._addPoint(np.array([ 1, t, 0]))
+        self._addPoint(np.array([-1,-t, 0]))
+        self._addPoint(np.array([ 1,-t, 0]))
+        self._addPoint(np.array([ 0,-1, t]))
+        self._addPoint(np.array([ 0, 1, t]))
+        self._addPoint(np.array([ 0,-1,-t]))
+        self._addPoint(np.array([ 0,-1,-t]))
+        self._addPoint(np.array([ t, 0,-1]))
+        self._addPoint(np.array([ t, 0, 1]))
+        self._addPoint(np.array([-t, 0,-1]))
+        self._addPoint(np.array([-t, 0, 1]))
         
         # make triangles
         tris = self.triangles
@@ -54,7 +54,7 @@ class IcoSphere(level):
         tris.append( Triangle([ verts[0],verts[10],verts[11]] ))
         # 5 adjacent faces
         tris.append( Triangle([ verts[1], verts[5], verts[9]] ))
-        tris.append( Triangle([ verts[5],verts[11],verts[14]] ))
+        tris.append( Triangle([ verts[5],verts[11], verts[4]] ))
         tris.append( Triangle([verts[11],verts[10], verts[2]] ))
         tris.append( Triangle([verts[10], verts[7], verts[6]] ))
         tris.append( Triangle([ verts[7], verts[1], verts[8]] ))
@@ -81,13 +81,13 @@ class IcoSphere(level):
             for tri in self.triangles:
                 # replace triangle by 4 triangles
                 p = tri.pts
-                p.append(_getMiddlePoint( tri.pts[0], tri.pts[1], midPointDict))
-                p.append(_getMiddlePoint( tri.pts[0], tri.pts[2], midPointDict))
-                p.append(_getMiddlePoint( tri.pts[1], tri.pts[2], midPointDict))
-                faces.append( Triangle( p[0], p[3], p[5]))
-                faces.append( Triangle( p[1], p[4], p[3]))
-                faces.append( Triangle( p[2], p[5], p[4]))
-                faces.append( Triangle( p[3], p[4], p[5]))
+                p.append(self._getMiddlePoint( tri.pts[0], tri.pts[1], midPointDict))
+                p.append(self._getMiddlePoint( tri.pts[0], tri.pts[2], midPointDict))
+                p.append(self._getMiddlePoint( tri.pts[1], tri.pts[2], midPointDict))
+                faces.append( Triangle( [ p[0], p[3], p[5]]))
+                faces.append( Triangle( [ p[1], p[4], p[3]]))
+                faces.append( Triangle( [ p[2], p[5], p[4]]))
+                faces.append( Triangle( [ p[3], p[4], p[5]]))
             # once looped thru all triangles overwrite self.triangles
             self.triangles = faces
     
@@ -95,7 +95,7 @@ class IcoSphere(level):
         """Add point to self.points"""
         self.points.append( Point( len(self.points), xyz))
         
-    def _getMiddlePoint(p1, p2, midPointDict):
+    def _getMiddlePoint(self, p1, p2, midPointDict):
         """return Point"""
         if not isinstance(p1,Point) or not isinstance(p2,Point):
             raise TypeError('p1 and p2 must be Points')        
@@ -106,28 +106,30 @@ class IcoSphere(level):
             pass
         else:
             # point is new
-            _addPoint( (p1.xyz + p2.xyz)/2)
+            self._addPoint( (p1.xyz + p2.xyz)/2)
             midPointDict[key] = self.points[-1] 
         return midPointDict[key]
         
         
-class Triangle(self,pts):
+class Triangle:
     """A triangle adjoining three adjacent points"""
     def __init__(self, pts):              
-        if isinstance(pts, list):
-            self.pts = pts
-        else:
+        if not isinstance(pts, list):
             raise TypeError('pts must be a list')
+        elif len(pts) !=3:
+            raise Exception('pts must be of length 3')    
+        else:
+            self.pts = pts
 
-class Point(self, idx, xyz):
+class Point:
     """A 3D point on the mesh"""
     def __init__(self, idx, xyz):         
         
         if type(idx) is not int:
             raise TypeError('idx must be an integer')              
-        else if not isinstance(xyz,np.ndarray):
+        elif not isinstance(xyz,np.ndarray):
             raise TypeError('xyz must be a numpy array')
-        else if xyz.size != 3:
+        elif xyz.size != 3:
             raise Exception('xyz must be of size 3')
         else:
             # ensure length equals 1 and add to list of points
