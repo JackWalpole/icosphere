ba6f393037e9db33268d462e8d2cb6be08121efa
Jack <jack.walpole@bristol.ac.uk>
2017-06-22 19:35:46 +0100

    More bug fixes -- code returns expected numpber of nodes and faces

--- icosahedron/icosahedron.py
+++ icosahedron/icosahedron.py
@@ -15,15 +15,18 @@ class IcoSphere:
     def __init__(self, level):       
         if type(level) is not int:
             raise TypeError('level must be an integer')
+        elif level < 0:
+            raise Exception('level must be no less than 0')
         elif level > self.maxlevel:
-            raise Exception('level must be no larger than ' + str(maxlevel))
+            raise Exception('level larger than ' + str(self.maxlevel) + ' not supported')
         
         self.level = level
         self.points = []
         self.triangles = []
+        self.npts = 0
         
         ################################
-        # initialise level 0 icosahedron
+        # initialise level 1 icosahedron
         ################################
         
         # golden ration
@@ -47,66 +50,77 @@ class IcoSphere:
         tris = self.triangles
         verts = self.points
         # 5 faces around point 0
-        tris.append( Triangle([ verts[0],verts[11], verts[5]] ))
-        tris.append( Triangle([ verts[0], verts[5], verts[1]] ))
-        tris.append( Triangle([ verts[0], verts[1], verts[7]] ))
-        tris.append( Triangle([ verts[0], verts[7],verts[10]] ))
-        tris.append( Triangle([ verts[0],verts[10],verts[11]] ))
+        tris.append(Triangle([ verts[0],verts[11], verts[5]]))
+        tris.append(Triangle([ verts[0], verts[5], verts[1]]))
+        tris.append(Triangle([ verts[0], verts[1], verts[7]]))
+        tris.append(Triangle([ verts[0], verts[7],verts[10]]))
+        tris.append(Triangle([ verts[0],verts[10],verts[11]]))
         # 5 adjacent faces
-        tris.append( Triangle([ verts[1], verts[5], verts[9]] ))
-        tris.append( Triangle([ verts[5],verts[11], verts[4]] ))
-        tris.append( Triangle([verts[11],verts[10], verts[2]] ))
-        tris.append( Triangle([verts[10], verts[7], verts[6]] ))
-        tris.append( Triangle([ verts[7], verts[1], verts[8]] ))
+        tris.append(Triangle([ verts[1], verts[5], verts[9]]))
+        tris.append(Triangle([ verts[5],verts[11], verts[4]]))
+        tris.append(Triangle([verts[11],verts[10], verts[2]]))
+        tris.append(Triangle([verts[10], verts[7], verts[6]]))
+        tris.append(Triangle([ verts[7], verts[1], verts[8]]))
         # 5 faces around point 3
-        tris.append( Triangle([ verts[3], verts[9], verts[4]] ))
-        tris.append( Triangle([ verts[3], verts[4], verts[2]] ))
-        tris.append( Triangle([ verts[3], verts[2], verts[6]] ))
-        tris.append( Triangle([ verts[3], verts[6], verts[8]] ))
-        tris.append( Triangle([ verts[3], verts[8], verts[9]] ))
+        tris.append(Triangle([ verts[3], verts[9], verts[4]]))
+        tris.append(Triangle([ verts[3], verts[4], verts[2]]))
+        tris.append(Triangle([ verts[3], verts[2], verts[6]]))
+        tris.append(Triangle([ verts[3], verts[6], verts[8]]))
+        tris.append(Triangle([ verts[3], verts[8], verts[9]]))
         # 5 adjacent faces
-        tris.append( Triangle([ verts[4], verts[9], verts[5]] ))
-        tris.append( Triangle([ verts[2], verts[4],verts[11]] ))
-        tris.append( Triangle([ verts[6], verts[2],verts[10]] ))
-        tris.append( Triangle([ verts[8], verts[6], verts[7]] ))
-        tris.append( Triangle([ verts[9], verts[8], verts[1]] ))
+        tris.append(Triangle([ verts[4], verts[9], verts[5]]))
+        tris.append(Triangle([ verts[2], verts[4],verts[11]]))
+        tris.append(Triangle([ verts[6], verts[2],verts[10]]))
+        tris.append(Triangle([ verts[8], verts[6], verts[7]]))
+        tris.append(Triangle([ verts[9], verts[8], verts[1]]))
         
         ########################################
         # refine triangles to desired mesh level
         ########################################
         
-        for l in range(1, self.level):
+        for l in range(self.level):
             midPointDict = {}
             faces = []
             for tri in self.triangles:
                 # replace triangle by 4 triangles
                 p = tri.pts
-                p.append(self._getMiddlePoint( tri.pts[0], tri.pts[1], midPointDict))
-                p.append(self._getMiddlePoint( tri.pts[0], tri.pts[2], midPointDict))
-                p.append(self._getMiddlePoint( tri.pts[1], tri.pts[2], midPointDict))
-                faces.append( Triangle( [ p[0], p[3], p[5]]))
-                faces.append( Triangle( [ p[1], p[4], p[3]]))
-                faces.append( Triangle( [ p[2], p[5], p[4]]))
-                faces.append( Triangle( [ p[3], p[4], p[5]]))
+                a = self._getMiddlePoint(p[0], p[1], midPointDict)
+                b = self._getMiddlePoint(p[1], p[2], midPointDict)
+                c = self._getMiddlePoint(p[2], p[0], midPointDict)
+                faces.append(Triangle([p[0], a, c]))
+                faces.append(Triangle([p[1], b, a]))
+                faces.append(Triangle([p[2], c, b]))
+                faces.append(Triangle([a, b, c]))
             # once looped thru all triangles overwrite self.triangles
             self.triangles = faces
+        
+        self.nfaces = len(self.triangles)
+            
+        # check that npts and nfaces are as expected
+        expected_npts = calculate_npts(self.level)
+        expected_nfaces = calculate_nfaces(self.level)
+        if self.npts != calculate_npts(self.level):
+            raise Exception('npts '+str(self.npts)+' not as expected '+str(expected_npts))
+        elif self.nfaces != calculate_nfaces(self.level):
+            raise Exception('nfaces '+str(self.nfaces)+' not as expected '+str(expected_nfaces))
     
     def _addPoint(self, xyz):
         """Add point to self.points"""
-        self.points.append( Point( len(self.points), xyz))
+        self.points.append(Point(self.npts, xyz))
+        self.npts += 1
         
     def _getMiddlePoint(self, p1, p2, midPointDict):
         """return Point"""
-        if not isinstance(p1,Point) or not isinstance(p2,Point):
+        if not isinstance(p1, Point) or not isinstance(p2, Point):
             raise TypeError('p1 and p2 must be Points')        
         # does point already exist?
-        key = tuple( sorted( [p1.idx, p2.idx]))
+        key = tuple(sorted([p1.idx, p2.idx]))
         if key in midPointDict:
             # point exists
             pass
         else:
             # point is new
-            self._addPoint( (p1.xyz + p2.xyz)/2)
+            self._addPoint((p1.xyz + p2.xyz)/2)
             midPointDict[key] = self.points[-1] 
         return midPointDict[key]
         
@@ -133,9 +147,15 @@ class Point:
             raise Exception('xyz must be of size 3')
         else:
             # ensure length equals 1 and add to list of points
-            self.xyz = ( xyz / np.linalg.norm(xyz))
+            self.xyz = (xyz/np.linalg.norm(xyz))
             self.idx = idx 
         
-
+def calculate_npts(level):
+    n = 2**level
+    return 2 + 10 * n**2
+    
+def calculate_nfaces(level):
+    n = 2**level
+    return 20 * n**2
     
     
