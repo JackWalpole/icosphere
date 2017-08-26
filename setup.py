fd42cad512daec7ed526223918b6be9e70433727
Jack Walpole <j.walpole@gmail.com>
2017-08-26 02:32:10 +0100

    setup setup.py following the splitwavepy example

--- setup.py
+++ setup.py
@@ -5,10 +5,14 @@ except ImportError:
 
 config = {
     'description': 'Tools for handling icosahedral spheres',
+    'author': 'Jack Walpole',
+    'author_email' = 'j,walpole@gmail.com',
+    'license' = 'MIT',
+    'url' = 'https://github.com/JackWalpole/icosphere' 
     'version': '0.1.0',
-    'install_requires': [],
-    'packages': ['icosahedron'],
-    'name': 'icosahedron'
+    'install_requires': ['numpy >= 1.1'],
+    'packages': ['icosphere'],
+    'name': 'icosphere'
 }
 
 setup(**config)
